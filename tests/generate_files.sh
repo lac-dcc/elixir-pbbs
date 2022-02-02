9ea638bf95cbda309e342162759b78f998ad02c2
Jean George <jean.george.alves@gmail.com>
2022-02-02 16:52:12 -0300

    Added tests generator

---
+++ tests/generate_files.sh
@@ -0,0 +1,20 @@
+declare -a comparison_sort_type=(
+    "almost_sorted_seq"
+    "expt_seq"
+    "random_seq"
+    "trigram_seq"
+)
+
+declare -a integer_sort_type=(
+    "expt_seq"
+    "random_seq"
+)
+
+for i in {1..7}; do
+    for type in "${comparison_sort_type[@]}"; do
+        ./build/$type -t double $((10**$i)) comparison_sort/$type/$((10**$i))
+    done
+    for type in "${integer_sort_type[@]}"; do
+        ./build/$type -t int $((10**$i)) integer_sort/$type/$((10**$i))
+    done
+done
