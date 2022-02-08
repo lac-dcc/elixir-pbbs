dc859101bc61cde5a83183815cc6a38feb1fa158
Jean George <jean.george.alves@gmail.com>
2022-02-08 09:55:54 -0300

    Added frequency to tests

--- tests/integer_sort/test.sh
+++ tests/integer_sort/test.sh
@@ -3,31 +3,40 @@ declare -a integer_sort_type=(
     "random_seq"
 )
 
+declare -a frequencies=(
+    "1.6GHz"
+    "1.8GHz"
+)
+
 size=0
 mix escript.build
 
-csv_output=$'array_type\tarray_size\tcpu_cores\telapsed_time'
-# filename=benchmarkings/integer_sort/data_$(date +'%Y%m%d%H%M%S').csv
-filename=benchmarkings/integer_sort/data.csv
+csv_output=$'array_type\tarray_size\tfrequency\tcpu_cores\telapsed_time'
+filename=benchmarkings/integer_sort/data_$(date +'%Y-%m-%d').csv
+# filename=benchmarkings/integer_sort/data.csv
 
 rm $filename
 echo "$csv_output" >> $filename
 
 for type in "${integer_sort_type[@]}"; do
-    for exp in {1..7}; do
-        cores="0"
-        for core in {0..7}; do
-            if [ "$core" != "0" ]; then
-                cores="$cores,$core"
-            fi
-            cores_number=$((core+1))
-            for round in {1..5}; do
-                size=$((10**exp))
-                cmd="taskset -c $cores escript pbbs RadixSort 10 https://monography.s3.us-east-2.amazonaws.com/integer_sort/$type/$size.txt"
-                echo $cmd
-                result=$($cmd)
-                echo $result
-                echo -e "$type\t$size\t$cores_number\t$result" >> $filename
+    for exp in {1..6}; do
+        for freq in "${frequencies[@]}"; do
+            freq_cmd="sudo cpufreq-set -c 4 -f $freq"
+            echo $freq_cmd
+            cores="0"
+            for core in {0..7}; do
+                if [ "$core" != "0" ]; then
+                    cores="$cores,$core"
+                fi
+                cores_number=$((core+1))
+                for round in {1..5}; do
+                    size=$((10**exp))
+                    cmd="taskset -c $cores escript pbbs RadixSort 10 https://monography.s3.us-east-2.amazonaws.com/integer_sort/$type/$size.txt"
+                    echo $cmd
+                    result=$($cmd)
+                    echo $result
+                    echo -e "$type\t$size\t$freq\t$cores_number\t$result" >> $filename
+                done
             done
         done
     done
