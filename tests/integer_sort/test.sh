597915231e614208be189ba1d79f305beca47356
Jean George <jean.george.alves@gmail.com>
2022-02-09 20:35:16 -0300

    Fixed integer sort test to work with correct cpu core combinations

--- tests/integer_sort/test.sh
+++ tests/integer_sort/test.sh
@@ -1,41 +1,29 @@
-declare -a integer_sort_type=(
-    "expt_seq"
-    "random_seq"
-)
-
-declare -a frequencies=(
-    "1.6GHz"
-    "1.8GHz"
-)
+integer_sort_type="expt_seq random_seq"
+frequencies="1.6GHz 1.8GHz"
+configs="0x09 0x60 0x69 0xf0 0x0f 0xff"
 
 size=0
 mix escript.build
 
-csv_output=$'array_type\tarray_size\tfrequency\tcpu_cores\telapsed_time'
-# filename=benchmarkings/integer_sort/data_$(date +'%Y-%m-%d').csv
-filename=benchmarkings/integer_sort/data.csv
+csv_output=$'array_type\tarray_size\tfrequency\tconfig\telapsed_time'
+filename=benchmarkings/integer_sort/data_$(date +'%Y-%m-%d').csv
 
-rm $filename
+rm -f $filename
 echo "$csv_output" >> $filename
 
-for type in "${integer_sort_type[@]}"; do
+for type in $integer_sort_type; do
     for exp in {1..6}; do
-        for freq in "${frequencies[@]}"; do
+        for freq in $frequencies; do
             freq_cmd="sudo cpufreq-set -c 4 -f $freq"
             echo $freq_cmd
-            cores="0"
-            for core in {0..7}; do
-                if [ "$core" != "0" ]; then
-                    cores="$cores,$core"
-                fi
-                cores_number=$((core+1))
+            for config in $configs; do
                 for round in {1..5}; do
                     size=$((10**exp))
-                    cmd="taskset -c $cores escript pbbs RadixSort 10 https://monography.s3.us-east-2.amazonaws.com/integer_sort/$type/$size.txt"
+                    cmd="taskset -c $config escript pbbs RadixSort 10 https://monography.s3.us-east-2.amazonaws.com/integer_sort/$type/$size.txt"
                     echo $cmd
                     result=$($cmd)
                     echo $result
-                    echo -e "$type\t$size\t$freq\t$cores_number\t$result" >> $filename
+                    echo -e "$type\t$size\t$freq\t$config\t$result" >> $filename
                 done
             done
         done
