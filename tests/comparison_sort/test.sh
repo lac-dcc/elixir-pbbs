6ddac2bd9bf0803e17ea4f3dee782fb3432a4435
Jean George <jean.george.alves@gmail.com>
2022-02-10 15:13:06 -0300

    Tests refactored

--- tests/comparison_sort/test.sh
+++ tests/comparison_sort/test.sh
@@ -6,38 +6,40 @@ configs="0x09 0x60 0x69 0xf0 0x0f 0xff"
 size=0
 mix escript.build
 
-csv_output=$'array_type\tarray_size\tlower_limit\tfrequency\tconfig\telapsed_time'
+header=$'array_type\tconfig\tfrequency\tarray_size\tlower_limit\telapsed_time'
 dir=benchmarkings/comparison_sort
-filename=$dir/data_$(date +'%Y-%m-%d-%H-%M-%S').csv
-
-mkdir -p $dir
-rm -f $filename
-echo "$csv_output" >> $filename
 
 for type in $comparison_sort_type; do
-    for exp in {1..5}; do
-        lower_limit=1
-        size=$((10**exp))
-        while : ; do
-            lower_limit=$((lower_limit*2))
-            if [ $lower_limit -gt $size ]; then
-                break
-            fi
-            for freq in $frequencies; do
-                freq_cmd="sudo cpufreq-set -c 4 -f $freq"
-                echo $freq_cmd
-                for config in $configs; do
+    for config in $configs; do
+        for freq in $frequencies; do
+            filedir=$dir/$type/$config/$freq
+            mkdir -p $filedir
+            filename=$filedir/$type-$config-$freq.csv
+            rm $filename
+            echo "$header" >> $filename
+            freq_cmd="sudo cpufreq-set -c 4 -f $freq"
+            echo $freq_cmd
+            for exp in {1..6}; do
+                size=$((10**exp))
+                lower_limit=1
+                if [ $size = 1000000 ]; then
+                    lower_limit=$((2**14))
+                fi
+                while : ; do
+                    lower_limit=$((lower_limit*2))
+                    if [ $lower_limit -gt $size ]; then
+                        break
+                    fi
                     # for round in {1..5}; do
                     # cmd="taskset $config escript pbbs SampleSort 10 $lower_limit https://monography.s3.us-east-2.amazonaws.com/comparison_sort/$type/$size.txt"
                     cmd="taskset $config escript pbbs SampleSort 10 $lower_limit tests/integer_sort/$type/$size.txt"
                     echo $cmd
                     result=$($cmd)
                     echo $result
-                    echo -e "$type\t$size\t$lower_limit\t$freq\t$config\t$result" >> $filename
+                    echo -e "$type\t$config\t$freq\t$size\t$lower_limit\t$result" >> $filename
                     # done
                 done
             done
         done
-        # done
     done
 done
--- tests/integer_sort/test.sh
+++ tests/integer_sort/test.sh
@@ -5,29 +5,29 @@ configs="0x09 0x60 0x69 0xf0 0x0f 0xff"
 size=0
 mix escript.build
 
-csv_output=$'array_type\tarray_size\tfrequency\tconfig\telapsed_time'
-dir=benchmarkings/comparison_sort
-filename=$dir/data_$(date +'%Y-%m-%d').csv
-
-mkdir -p $dir
-rm -f $filename
-echo "$csv_output" >> $filename
+header=$'array_type\tconfig\tfrequency\tarray_size\telapsed_time'
+dir=benchmarkings/integer_sort
 
 for type in $integer_sort_type; do
-    for exp in {1..6}; do
+    for config in $configs; do
         for freq in $frequencies; do
+            filedir=$dir/$type/$config/$freq
+            mkdir -p $filedir
+            filename=$filedir/$type-$config-$freq.csv
+            rm $filename
+            echo "$header" >> $filename
             freq_cmd="sudo cpufreq-set -c 4 -f $freq"
             echo $freq_cmd
-            for config in $configs; do
-                # for round in {1..5}; do
+            for exp in {1..6}; do
                 size=$((10**exp))
-                # cmd="taskset $config escript pbbs RadixSort 10 https://monography.s3.us-east-2.amazonaws.com/integer_sort/$type/$size.txt"
-                cmd="taskset $config escript pbbs RadixSort 10 tests/integer_sort/$type/$size.txt"
-                echo $cmd
-                result=$($cmd)
-                echo $result
-                echo -e "$type\t$size\t$freq\t$config\t$result" >> $filename
-                # done
+                for round in {1..5}; do
+                    # cmd="taskset $config escript pbbs RadixSort 10 https://monography.s3.us-east-2.amazonaws.com/integer_sort/$type/$size.txt"
+                    cmd="taskset $config escript pbbs RadixSort 10 tests/integer_sort/$type/$size.txt"
+                    echo $cmd
+                    result=$($cmd)
+                    echo $result
+                    echo -e "$type\t$config\t$freq\t$size\t$result" >> $filename
+                done
             done
         done
     done
