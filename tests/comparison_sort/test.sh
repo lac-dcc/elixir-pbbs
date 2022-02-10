00cbccab657f54e0cee9f914ac6fb3ce3a9c7bd7
Jean George <jean.george.alves@gmail.com>
2022-02-10 12:48:30 -0300

    Fixed comparison sort tests

--- tests/comparison_sort/test.sh
+++ tests/comparison_sort/test.sh
@@ -1,4 +1,5 @@
-comparison_sort_type="almost_sorted_seq expt_seq random_seq trigram_seq"
+# comparison_sort_type="almost_sorted_seq expt_seq random_seq trigram_seq"
+comparison_sort_type="expt_seq random_seq"
 frequencies="1.6GHz 1.8GHz"
 configs="0x09 0x60 0x69 0xf0 0x0f 0xff"
 
@@ -7,15 +8,14 @@ mix escript.build
 
 csv_output=$'array_type\tarray_size\tlower_limit\tfrequency\tconfig\telapsed_time'
 dir=benchmarkings/comparison_sort
-filename=$dir/data_$(date +'%Y-%m-%d').csv
+filename=$dir/data_$(date +'%Y-%m-%d-%H-%M-%S').csv
 
 mkdir -p $dir
 rm -f $filename
 echo "$csv_output" >> $filename
 
 for type in $comparison_sort_type; do
-
-    for exp in {1..6}; do
+    for exp in {1..5}; do
         lower_limit=1
         size=$((10**exp))
         while : ; do
@@ -29,7 +29,7 @@ for type in $comparison_sort_type; do
                 for config in $configs; do
                     for round in {1..5}; do
                         # cmd="taskset $config escript pbbs SampleSort 10 $lower_limit https://monography.s3.us-east-2.amazonaws.com/comparison_sort/$type/$size.txt"
-                        cmd="taskset $config escript pbbs SampleSort 10 $lower_limit tests/comparison_sort/$type/$size.txt"
+                        cmd="taskset $config escript pbbs SampleSort 10 $lower_limit tests/integer_sort/$type/$size.txt"
                         echo $cmd
                         result=$($cmd)
                         echo $result
