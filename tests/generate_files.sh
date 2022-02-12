a032755d7c78bff5fb626ad679f5382fc7ff13fa
jeanGeorge <jean.george.alves@gmail.com>
2022-02-12 00:33:01 -0300

    Added new files to test

--- tests/generate_files.sh
+++ tests/generate_files.sh
@@ -1,20 +1,12 @@
-declare -a comparison_sort_type=(
-    "almost_sorted_seq"
-    "expt_seq"
-    "random_seq"
-    "trigram_seq"
-)
+comparison_sort_type="almost_sorted_seq expt_seq random_seq trigram_seq"
+integer_sort_type="expt_seq random_seq"
+sizes="10 100 1000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000"
 
-declare -a integer_sort_type=(
-    "expt_seq"
-    "random_seq"
-)
-
-for i in {1..7}; do
-    for type in "${comparison_sort_type[@]}"; do
-        ./build/$type -t double $((10**$i)) comparison_sort/$type/$((10**$i)).txt
+for size in $sizes; do
+    for type in $comparison_sort_type; do
+        ./build/$type -t double $size comparison_sort/$type/$size.txt
     done
-    for type in "${integer_sort_type[@]}"; do
-        ./build/$type -t int $((10**$i)) integer_sort/$type/$((10**$i)).txt
+    for type in $integer_sort_type; do
+        ./build/$type -t int $size integer_sort/$type/$size.txt
     done
 done
