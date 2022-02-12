15a3816ebbc7b9ede55f8b197e49d3429f45e084
Jean George <jean.george.alves@gmail.com>
2022-02-12 02:47:15 -0300

    Refactored comparison sort benchmarking

--- tests/comparison_sort/test.sh
+++ tests/comparison_sort/test.sh
@@ -1,43 +1,40 @@
 # comparison_sort_type="almost_sorted_seq expt_seq random_seq trigram_seq"
-comparison_sort_type="expt_seq random_seq"
+# comparison_sort_type="expt_seq random_seq"
+comparison_sort_type="random_seq"
 frequencies="1.6GHz 1.8GHz"
 configs="0x09 0x60 0x69 0xf0 0x0f 0xff"
+sizes="10 100 1000 10000 20000 30000 40000 50000 60000 70000 80000 90000 100000"
 
-size=0
-mix escript.build
-
-header=$'array_type\tconfig\tfrequency\tarray_size\tlower_limit\telapsed_time'
+header=$'array_type\tconfig\tfrequency\tarray_size\telapsed_time'
 dir=benchmarkings/comparison_sort
 
+mix escript.build
+
 for type in $comparison_sort_type; do
+    filedir=$dir/$type
+    mkdir -p $filedir
+    filename=$filedir/$(date +'%Y-%m-%d-%H-%M-%S').csv
+    rm $filename
+    echo "$header" >> $filename
     for config in $configs; do
         for freq in $frequencies; do
-            filedir=$dir/$type/$config/$freq
-            mkdir -p $filedir
-            filename=$filedir/$type-$config-$freq.csv
-            rm $filename
-            echo "$header" >> $filename
             freq_cmd="sudo cpufreq-set -c 4 -f $freq"
             echo $freq_cmd
-            for exp in {1..5}; do
-                size=$((10**exp))
+            for size in $sizes; do
                 lower_limit=1
-                if [ $size = 1000000 ]; then
-                    lower_limit=$((2**14))
-                fi
                 while : ; do
                     lower_limit=$((lower_limit*2))
                     if [ $lower_limit -gt $size ]; then
                         break
                     fi
-                    for round in {1..5}; do
-                        # cmd="taskset $config escript pbbs SampleSort 10 $lower_limit https://monography.s3.us-east-2.amazonaws.com/comparison_sort/$type/$size.txt"
-                        cmd="taskset $config escript pbbs SampleSort 10 $lower_limit tests/integer_sort/$type/$size.txt"
-                        echo $cmd
-                        result=$($cmd)
-                        echo $result
-                        echo -e "$type\t$config\t$freq\t$size\t$lower_limit\t$result" >> $filename
-                    done
+                    # for round in {1..5}; do
+                    # cmd="taskset $config escript pbbs SampleSort 10 $lower_limit https://monography.s3.us-east-2.amazonaws.com/comparison_sort/$type/$size.txt"
+                    cmd="taskset $config escript pbbs SampleSort 10 $lower_limit tests/integer_sort/$type/$size.txt"
+                    echo $cmd
+                    result=$($cmd)
+                    echo $result
+                    echo -e "$type\t$config\t$freq\t$size\t$lower_limit\t$result" >> $filename
+                    # done
                 done
             done
         done
