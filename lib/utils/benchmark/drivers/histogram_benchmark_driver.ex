ef340ebc1cc5106bb6a1007f0827214bf269c31e
Luiz Berto <diasbertoluiz@gmail.com>
2022-09-23 20:04:02 -0300

    use Benchee for benchmarks

--- lib/utils/benchmark/drivers/histogram_benchmark_driver.ex
+++ lib/utils/benchmark/drivers/histogram_benchmark_driver.ex
@@ -1,27 +1,24 @@
 defmodule Utils.HistogramBenchmarkDriver do
+
   def run_benchmark(implementations, processors) do
+    IO.inspect(implementations)
+    buckets = 1000
+    size = 300_000
+    data = Utils.Generators.random_sequence(10, size)
+
     impl_map = %{
-      "serial" => fn (data, buckets) -> Sequences.Histogram.histogram(data, buckets) end,
-      "actors" => fn (data, buckets) -> Sequences.Histogram.Parallel.histogram(data, buckets) end,
-      "dc" => fn (data, buckets) -> Sequences.Histogram.Parallel.DivideAndConquer.histogram(data, buckets, processors) end,
-      #"dc_tuples" => &Sequences.Histogram.Parallel.DivideAndConquer.Tuples.histogram/2
+      "serial" => fn -> Sequences.Histogram.histogram(data, buckets) end,
+      "actors" => fn -> Sequences.Histogram.Parallel.histogram(data, buckets) end,
+      "dc" => fn -> Sequences.Histogram.Parallel.DivideAndConquer.histogram(data, buckets, processors) end,
+      #"dc_tuples" => fn -> Sequences.Histogram.Parallel.DivideAndConquer.Tuples.histogram(data, buckets) end,
       # TODO: handle implementations that deviate from the default signature (introduce new level of abstraction?)
     }
 
-    times = Enum.map(implementations, fn impl ->
-      {impl, exec_benchmark(impl_map[impl])}
+    to_run = Enum.filter(impl_map, fn ({key, _value}) ->
+      IO.puts(key)
+      MapSet.member?(implementations, key)
     end)
 
-    IO.inspect(times)
-  end
-
-  defp exec_benchmark(fun) do
-    buckets = 10
-    size = 30_000_000
-
-    input = Utils.Generators.random_sequence(10, size)
-
-    {time, _ret} = Benchmark.measure(fn -> fun.(input, buckets) end)
-    time / 1000 # ms
+    Benchee.run(to_run)
   end
 end
