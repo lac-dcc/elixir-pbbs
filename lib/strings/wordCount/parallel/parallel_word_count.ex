9b23a3c7f85290acafc946f544b0b6cb7612aa3c
Luiz Berto <diasbertoluiz@gmail.com>
2022-10-23 10:41:47 -0300

    word count: lowercase words and ignore non-alpha characters

--- lib/strings/wordCount/parallel/parallel_word_count.ex
+++ lib/strings/wordCount/parallel/parallel_word_count.ex
@@ -3,13 +3,14 @@ defmodule Strings.WordCount.Parallel do
     (0..p-1)
     |> Enum.map(fn i ->
       Task.async(fn ->
-        words = String.split(string)
-        Enum.drop(words, i)
+        String.split(string, ~r/[^A-z]+/)
+        |> Enum.map(&String.downcase/1)
+        |> Enum.drop(i)
         |> Enum.take_every(p)
         |> Enum.frequencies
       end)
     end)
-    |> Task.await_many
+    |> Task.await_many(:infinity)
     |> Enum.reduce(%{}, fn (res, acc) ->
       Map.merge(res, acc, fn (_key, v1, v2) -> v1 + v2 end)
     end)
--- lib/strings/wordCount/sequential/sequential_word_count.ex
+++ lib/strings/wordCount/sequential/sequential_word_count.ex
@@ -1,6 +1,7 @@
 defmodule Strings.WordCount do
   def word_count(string) do
-    String.split(string)
+    String.split(string, ~r/[^A-z]+/)
+    |> Enum.map(&String.downcase/1)
     |> Enum.frequencies()
   end
 end
