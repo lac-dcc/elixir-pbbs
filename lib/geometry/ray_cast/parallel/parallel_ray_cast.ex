849c34075615351771b6505c1bf3982b09c8a865
Luiz Berto <diasbertoluiz@gmail.com>
2022-11-27 19:35:59 -0300

    optimize parallel ray cast

--- lib/geometry/ray_cast/parallel/parallel_ray_cast.ex
+++ lib/geometry/ray_cast/parallel/parallel_ray_cast.ex
@@ -2,11 +2,14 @@ defmodule Geometry.RayCast.Parallel.ParallelRayCast do
   def ray_cast(triangles_file_path, rays_file_path) do
     {triangles, rays} = Geometry.RayCast.ReadInput.read_input(triangles_file_path, rays_file_path)
 
+    :ets.new(:rc, [:public, :named_table])
     indexed_triangles = Enum.with_index(triangles)
+    :ets.insert(:rc, {:triangles, indexed_triangles})
 
     result = Enum.map(rays, fn ray ->
       Task.async(fn ->
-        Enum.map(indexed_triangles, fn ({triangle, index}) ->
+        tri = Keyword.get(:ets.lookup(:rc, :triangles), :triangles)
+        Enum.map(tri, fn ({triangle, index}) ->
           {Geometry.RayCast.ray_triangle_intersect(ray, triangle), index}
         end)
         |> Enum.filter(fn ({{intersects, _distance}, _index}) -> intersects end)
@@ -15,6 +18,8 @@ defmodule Geometry.RayCast.Parallel.ParallelRayCast do
       end)
     end)
     |> Task.await_many
+
+    :ets.delete(:rc)
     result
   end
 
