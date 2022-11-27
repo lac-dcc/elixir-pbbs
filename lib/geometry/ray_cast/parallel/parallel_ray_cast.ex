bd5d1579e25315d3bc88d96dd15e69e6b8eba32b
Luiz Berto <diasbertoluiz@gmail.com>
2022-11-27 19:55:47 -0300

    optimize parallel ray cast further

--- lib/geometry/ray_cast/parallel/parallel_ray_cast.ex
+++ lib/geometry/ray_cast/parallel/parallel_ray_cast.ex
@@ -1,23 +1,34 @@
 defmodule Geometry.RayCast.Parallel.ParallelRayCast do
   def ray_cast(triangles_file_path, rays_file_path) do
+    p = 11
+
     {triangles, rays} = Geometry.RayCast.ReadInput.read_input(triangles_file_path, rays_file_path)
 
     :ets.new(:rc, [:public, :named_table])
     indexed_triangles = Enum.with_index(triangles)
     :ets.insert(:rc, {:triangles, indexed_triangles})
+    :ets.insert(:rc, {:rays, rays})
 
-    result = Enum.map(rays, fn ray ->
+    size = ceil(length(rays) / p)
+    result = (0..p)
+    |> Enum.map(fn idx -> (idx*size) end)
+    |> Enum.map(fn start ->
       Task.async(fn ->
         tri = Keyword.get(:ets.lookup(:rc, :triangles), :triangles)
-        Enum.map(tri, fn ({triangle, index}) ->
-          {Geometry.RayCast.ray_triangle_intersect(ray, triangle), index}
+        Keyword.get(:ets.lookup(:rc, :rays), :rays)
+        |> Enum.slice(start, size)
+        |> Enum.map(fn ray ->
+          Enum.map(tri, fn ({triangle, index}) ->
+            {Geometry.RayCast.ray_triangle_intersect(ray, triangle), index}
+          end)
+          |> Enum.filter(fn ({{intersects, _distance}, _index}) -> intersects end)
+          |> Enum.min_by(fn ({{_intersects, distance}, _index}) -> distance end, fn -> -1 end)
+          |> take_index
         end)
-        |> Enum.filter(fn ({{intersects, _distance}, _index}) -> intersects end)
-        |> Enum.min_by(fn ({{_intersects, distance}, _index}) -> distance end, fn -> -1 end)
-        |> take_index
       end)
     end)
-    |> Task.await_many
+    |> Task.await_many(:infinity)
+    |> :lists.append
 
     :ets.delete(:rc)
     result
