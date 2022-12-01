b6393fc390938e9c91738ef3535787cf5a11b209
Luiz Berto <diasbertoluiz@gmail.com>
2022-12-01 14:41:20 -0300

    change convex hull to return points indices

--- lib/geometry/convex_hull/convex_hull.ex
+++ lib/geometry/convex_hull/convex_hull.ex
@@ -1,13 +1,15 @@
 defmodule Geometry.ConvexHull.ConvexHull do
   def convex_hull(points) do
-    min_x = Enum.min_by(points, fn point -> point.x end)
-    max_x = Enum.max_by(points, fn point -> point.x end)
+    indexed_points = Enum.with_index(points)
 
-    hsplit(points, min_x, max_x) ++ hsplit(points, max_x, min_x)
+    min_x = Enum.min_by(indexed_points, fn ({point, _idx}) -> point.x end)
+    max_x = Enum.max_by(indexed_points, fn ({point, _idx}) -> point.x end)
+
+    hsplit(indexed_points, min_x, max_x) ++ hsplit(indexed_points, max_x, min_x)
   end
 
-  def hsplit(points, p1, p2) do
-    cross = Enum.map(points, fn pt -> cross_product(pt, p1, p2) end)
+  def hsplit(points, {p1, p1_index}, {p2, p2_index}) do
+    cross = Enum.map(points, fn ({pt, _idx}) -> cross_product(pt, p1, p2) end)
 
     zipped = Enum.zip(points, cross)
     packed = zipped
@@ -15,12 +17,12 @@ defmodule Geometry.ConvexHull.ConvexHull do
     |> Enum.map(fn ({point, _cross}) -> point end)
 
     if length(packed) < 2 do
-      [p1] ++ packed
+      [p1_index] ++ Enum.map(packed, fn ({_pt, index}) -> index end)
     else
       pm = Enum.max_by(zipped, fn ({_point, cross}) -> cross end)
       |> elem(0)
 
-      hsplit(packed, p1, pm) ++ hsplit(packed, pm, p2)
+      hsplit(packed, {p1, p1_index}, pm) ++ hsplit(packed, pm, {p2, p2_index})
     end
   end
 
