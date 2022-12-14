584ba3a0f3f099caee352b06be2dd5498e8ec06b
Luiz Berto <diasbertoluiz@gmail.com>
2022-12-14 14:56:58 -0300

    update dense text size

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -13,8 +13,8 @@ echo "Container id: $container_id"
 #docker exec -it "$container_id" sh -c "mix benchmark -a remove_duplicates"
 #docker cp "$container_id":/app/output_ddup.csv experiments_results/output_ddup.csv
 
-#docker exec -it "$container_id" sh -c "mix benchmark -a word_count"
-#docker cp "$container_id":/app/output_wc.csv experiments_results/output_wc.csv
+docker exec -it "$container_id" sh -c "mix benchmark -a word_count"
+docker cp "$container_id":/app/output_wc.csv experiments_results/output_wc.csv
 
 #docker exec -it "$container_id" sh -c "mix benchmark -a ray_cast"
 #docker cp "$container_id":/app/output_ray_cast.csv experiments_results/output_ray_cast.csv
@@ -25,8 +25,8 @@ echo "Container id: $container_id"
 #docker exec -it "$container_id" sh -c "mix benchmark -a suffix_array"
 #docker cp "$container_id":/app/output_suffix_array.csv experiments_results/output_suffix_array.csv
 
-docker exec -it "$container_id" sh -c "mix benchmark -a integer_sort"
-docker cp "$container_id":/app/output_integer_sort.csv experiments_results/output_integer_sort.csv
+#docker exec -it "$container_id" sh -c "mix benchmark -a integer_sort"
+#docker cp "$container_id":/app/output_integer_sort.csv experiments_results/output_integer_sort.csv
 
 #docker exec -it "$container_id" sh -c "mix benchmark -a comparison_sort"
 #docker cp "$container_id":/app/output_comparison_sort.csv experiments_results/output_comparison_sort.csv
