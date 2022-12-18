185458ecb15eea14512a66e5a45ba52a7ef4ca04
Luiz Berto <diasbertoluiz@gmail.com>
2022-12-18 10:18:30 -0300

    update comparison sort benchmark driver

--- lib/utils/benchmark/drivers/comparison_sort_benchmark.driver.ex
+++ lib/utils/benchmark/drivers/comparison_sort_benchmark.driver.ex
@@ -4,14 +4,17 @@ defmodule Utils.Benchmark.Drivers.ComparisonSort do
     dense_list = Utils.Generators.random_sequence(50, 1_000_000)
     sparse_list = Utils.Generators.random_sequence(1_000_000)
 
-    p = System.schedulers_online()
+    plist = [2, 4, 6, 12, 24, 32, 40]
 
-    impl_map = Map.new()
+    impl_map = Enum.flat_map(plist, fn p ->
+      [
+        {"parallel;p=#{p};dense_list", fn () -> PBBS.Sequences.ComparisonSort.Parallel.merge_sort(dense_list, p) end},
+        {"parallel;p=#{p};sparse_list", fn () -> PBBS.Sequences.ComparisonSort.Parallel.merge_sort(sparse_list, p) end},
+      ]
+    end)
+    |> Map.new()
     |> Map.put("serial;dense_list", fn () -> Enum.sort(dense_list) end)
     |> Map.put("serial;sparse_list", fn () -> Enum.sort(sparse_list) end)
-    |> Map.put("parallel;p=#{p};dense_list", fn () -> PBBS.Sequences.ComparisonSort.Parallel.merge_sort(dense_list, p) end)
-    |> Map.put("parallel;p=#{p};sparse_list", fn () -> PBBS.Sequences.ComparisonSort.Parallel.merge_sort(sparse_list, p) end)
-    |> Map.new()
 
     Benchee.run(
       impl_map,
