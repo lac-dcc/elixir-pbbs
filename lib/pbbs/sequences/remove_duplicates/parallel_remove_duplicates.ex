f5592f11c2c4abc165d5237b85c824b6d04f4d7b
Luiz Berto <diasbertoluiz@gmail.com>
2022-12-18 21:41:25 -0300

    Revert "update remove duplicates to use async_stream"
    This reverts commit c33bfa53328afdd9c74cd561e0df97e2b87260a8.

--- lib/pbbs/sequences/remove_duplicates/parallel_remove_duplicates.ex
+++ lib/pbbs/sequences/remove_duplicates/parallel_remove_duplicates.ex
@@ -1,20 +1,24 @@
 defmodule PBBS.Sequences.RemoveDuplicates.Parallel do
   def remove_duplicates(nums, p) do
-    chunk_size = 20000
+    :ets.new(:ddup, [:public, :named_table])
+    :ets.insert(:ddup, {:data, nums})
 
-    Stream.chunk_every(nums, chunk_size)
-    |> Task.async_stream(
-      fn elements ->
-        MapSet.new(elements)
-      end,
-      max_parallelism: p - 1,
-      ordered: false,
-      timeout: :infinity
-    )
-    |> Stream.map(fn {:ok, v} -> v end)
-    |> Enum.reduce(MapSet.new(), fn res, acc ->
+    ret=(0..p-1)
+    |> Enum.map(fn i ->
+      Task.async(fn ->
+        input = Keyword.get(:ets.lookup(:ddup, :data), :data)
+        Enum.drop(input, i)
+        |> Enum.take_every(p)
+        |> MapSet.new
+      end)
+    end)
+    |> Task.await_many
+    |> Enum.reduce(MapSet.new(), fn (res, acc) ->
       MapSet.union(res, acc)
     end)
-    |> Enum.to_list
+
+    :ets.delete(:ddup)
+
+    Enum.to_list(ret)
   end
 end
